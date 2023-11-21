a84d082e19d53f3181592aa451736e2a98cf031a
Dorion Boesch <dorion.boesch@gmail.com>
2023-11-20 22:03:44 -0700

    change

--- game.py
+++ game.py
@@ -12,3 +12,10 @@ class TicTacToe:
         number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
         for row in number_board:
             print('| ' + ' | '.join(row) + ' |')
+
+    def available_moves(self):
+        return [i for i, spot in enumerate(self.board) if spot == ' ']
+        # moves = []
+        # for (i, spot) in enumerate(self.print_board):
+        #     if spot == ' ':
+        #         moves.append(i)
